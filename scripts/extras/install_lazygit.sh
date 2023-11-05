02d163d8914656c16fa5654fd7d20276270e7ddf
Iivari Korhonen <iivari.korhonen@tamtron.fi>
2023-11-06 01:35:12 +0200

    feat: add lazygit install script

---
+++ scripts/extras/install_lazygit.sh
@@ -0,0 +1,22 @@
+#!/bin/bash
+
+CUR_DIR=$(pwd)
+TMP_DIR=$(mktemp -d)
+
+cd "$TMP_DIR"
+
+echo "Checking for latest version for lazygit."
+LAZYGIT_VERSION=$(curl -s "https://api.github.com/repos/jesseduffield/lazygit/releases/latest" | grep -Po '"tag_name": "v\K[^"]*')
+
+echo "Downloading lazygit $LAZYGIT_VERSION."
+curl -Lo lazygit.tar.gz "https://github.com/jesseduffield/lazygit/releases/latest/download/lazygit_${LAZYGIT_VERSION}_Linux_x86_64.tar.gz"
+
+echo "Unpacking lazygit."
+tar xf lazygit.tar.gz lazygit
+
+echo "Installing lazygit."
+sudo install lazygit /usr/local/bin
+
+echo "Cleaning up installation files."
+cd "$CUR_DIR"
+rm -rf "$TMP_DIR"
